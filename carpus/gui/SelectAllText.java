637eb4a843616cf0c3d4556c6b6fe8cabfca2657
carpus <carpus@c5887c0f-4ef3-0310-a687-d07a66b422c7>
2005-04-24 23:23:00 +0000

    git-svn-id: file:///home/dcarpus/code/svn/repos/rmk/trunk@17 c5887c0f-4ef3-0310-a687-d07a66b422c7

--- carpus/gui/SelectAllText.java
+++ carpus/gui/SelectAllText.java
@@ -3,9 +3,9 @@ package carpus.gui;
 import java.awt.event.*;
 import javax.swing.text.*;
 
-class SelectAllText implements FocusListener{
+public class SelectAllText implements FocusListener{
     JTextComponent field;
-    SelectAllText(JTextComponent field){this.field = field;  }    
+    public SelectAllText(JTextComponent field){this.field = field;  }    
     public void focusGained(FocusEvent e) {this.field.selectAll(); }
     public void focusLost(FocusEvent e) {this.field.select(0,0); };
 }
