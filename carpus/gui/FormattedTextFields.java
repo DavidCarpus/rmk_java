1eae6f30f49dde5154546f66127ba6178d98b58a
carpus <carpus@c5887c0f-4ef3-0310-a687-d07a66b422c7>
2005-04-24 23:22:51 +0000

    git-svn-id: file:///home/dcarpus/code/svn/repos/rmk/trunk@16 c5887c0f-4ef3-0310-a687-d07a66b422c7

--- carpus/gui/FormattedTextFields.java
+++ carpus/gui/FormattedTextFields.java
@@ -5,56 +5,67 @@ import javax.swing.*;
 import java.text.*;
 import javax.swing.text.*;
 
-public class FormattedTextFields{
-    public static JTextField getNameField(String text, int size){
-	FirstCharUpperCaseField results  = new FirstCharUpperCaseField(size, text);
-	results.addFocusListener(new SelectAllText(results));
+public class FormattedTextFields {
+	public static JTextField getNameField(String text, int size) {
+		FirstCharUpperCaseField results = new FirstCharUpperCaseField(size,
+				text);
+		results.addFocusListener(new SelectAllText(results));
 
-	if(text != null && text.length() > 0){
-	    results.setText(text);
-	}	
-	return results;
-    }
+		if (text != null && text.length() > 0) {
+			results.setText(text);
+		}
+		return results;
+	}
 
-    public static JFormattedTextField getPercentageField(double amt){
-	JFormattedTextField results = new JFormattedTextField(NumberFormat.getPercentInstance());
-	results.addFocusListener(new SelectAllText(results));
-  	results.setValue(new Double(0));
-	results.setColumns(5);
-	return results;
-    }
+	public static JFormattedTextField getPercentageField(double amt) {
+		JFormattedTextField results = new JFormattedTextField(NumberFormat
+				.getPercentInstance());
+		results.addFocusListener(new SelectAllText(results));
+		results.setValue(new Double(0));
+		results.setColumns(5);
+		return results;
+	}
 
-    public static JFormattedTextField getCurrencyField(double amt){
-//   throws Exception{
-	NumberFormat formatter    = NumberFormat.getCurrencyInstance();
-	JFormattedTextField results = new JFormattedTextField(formatter);
-	results.addFocusListener(new SelectAllText(results));
-  	results.setValue(new Double(0));
-	results.setColumns(7);
-	return results;
-    }
+	public static JFormattedTextField getCurrencyField(double amt) {
+		//   throws Exception{
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		JFormattedTextField results = new JFormattedTextField(formatter);
+		results.addFocusListener(new SelectAllText(results));
+		results.setValue(new Double(0));
+		results.setColumns(7);
+		return results;
+	}
 
-    public static JFormattedTextField getDateField() {
-    	DateFormat format = new SimpleDateFormat("MM/dd/yy");
-	format.setLenient(true);
-  	DateFormatter formatter = new DateFormatter(format);
-	JFormattedTextField results = new JFormattedTextField(formatter);
-	results.setMinimumSize(new Dimension(80,20));
-	results.setPreferredSize(new Dimension(80,20));
-	results.addFocusListener(new SelectAllText(results));
-//  	results.setValue(new java.util.Date());
-	return results;
-    }
+	public static JFormattedTextField getDateField() {
+		DateFormat format = new SimpleDateFormat("MM/dd/yy");
+		format.setLenient(true);
+		DateFormatter formatter = new DateFormatter(format);
+		JFormattedTextField results = new JFormattedTextField(formatter);
+		results.setMinimumSize(new Dimension(80, 20));
+		results.setPreferredSize(new Dimension(80, 20));
+		results.addFocusListener(new SelectAllText(results));
+		//  	results.setValue(new java.util.Date());
+		return results;
+	}
 
-//      public static JFormattedTextField getSSN() {// US Social Security number
-//  	try{
-//  	    MaskFormatter formatter = new MaskFormatter("###-##-####");
-//  	    formatter.setPlaceholderCharacter('_');
-//  	    return new JFormattedTextField(formatter);
-//  	} catch (Exception e){
-//  	    carpus.util.Logger.getInstance().logError("Creating SSN Field", e);
-//  	    return new JFormattedTextField();
-//  	}
-//      }
+	public static JFormattedTextField getNumberField() {
+		NumberFormat formatter = NumberFormat.getNumberInstance();
+		JFormattedTextField results = new JFormattedTextField(formatter);
+		results.setMinimumSize(new Dimension(80, 20));
+		results.setPreferredSize(new Dimension(80, 20));
+		results.addFocusListener(new SelectAllText(results));
+		return results;
+	}
+
+	//      public static JFormattedTextField getSSN() {// US Social Security number
+	//  	try{
+	//  	    MaskFormatter formatter = new MaskFormatter("###-##-####");
+	//  	    formatter.setPlaceholderCharacter('_');
+	//  	    return new JFormattedTextField(formatter);
+	//  	} catch (Exception e){
+	//  	    carpus.util.Logger.getInstance().logError("Creating SSN Field", e);
+	//  	    return new JFormattedTextField();
+	//  	}
+	//      }
 
 }
